83d2c86c7c4a8a6ffadc2747b9bb2a5eaaaf8e6b
Cody Roux <cody.roux@gmail.com>
2015-11-24 19:53:13 -0500

    Finished slides (provisional), cleaned up the intro

--- code/intro.lean
+++ code/intro.lean
@@ -7,39 +7,45 @@ help commands
 
 section intro
 
-parameters a b c : Prop
+parameters a b c d : Prop
 
 print notation ↔
 
 -- This is probably the most useful command in Lean
 print prefix iff
 
+-- Let's try a "isar-style" proof here
 lemma and_left_distrib : a ∧ (b ∨ c) ↔ (a ∧ b) ∨ (a ∧ c) := sorry
 
 reveal and_left_distrib
 
 print and_left_distrib
 
+-- Let's try a tactic proof here
+
 lemma or_imp : (a → c) → (b → d) →  (a ∨ b) → (c ∨ d) := sorry
 
 check and_of_and_of_imp_of_imp
 
-lemma and_imp {a b c d} : (a → c) → (b → d) →  (a ∧ b) → (c ∧ d) := sorry
+lemma and_imp : (a → c) → (b → d) →  (a ∧ b) → (c ∧ d) := sorry
 
 
 print prefix or
 
-lemma or_not_not_and {a b : Prop} : (¬ a) ∨ (¬ b) → ¬ (a ∧ b) := sorry
+lemma or_not_not_and : (¬ a) ∨ (¬ b) → ¬ (a ∧ b) := sorry
 
-lemma and_not_not_or {a b : Prop} : (¬ a) ∧ (¬ b) → ¬ (a ∨ b) := sorry
+lemma and_not_not_or : (¬ a) ∧ (¬ b) → ¬ (a ∨ b) := sorry
 
 -- This requires the excluded middle
 
-axiom em {a : Prop} : decidable a
-attribute em [instance]
+-- hypothesis em [instance] {a : Prop} : decidable a
+-- small hack:
+definition em [instance] {a : Prop} : decidable a := sorry
 
 print prefix decidable
 
-lemma not_and_or_not {a b : Prop} : ¬ (a ∧ b) → ¬ a ∨ ¬ b := _
+-- What kind of proof do you want here?
+lemma not_and_or_not : ¬ (a ∧ b) → ¬ a ∨ ¬ b := sorry
 
+end intro
 
